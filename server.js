1d0a5398e946e97e87394dd8c272ea9a134c0df8
amine feidi <amine.feidi@gmail.com>
2020-03-06 19:48:27 +0100

    First commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const parse = require('./server2parser');
+const parse = require('./parser');
 
 let finalData = [];
 
