0a65a7186bef8871cff031e60b423a2c52d5ad8c
amine feidi <amine.feidi@gmail.com>
2020-03-11 01:43:14 +0100

    modified:   parser.js 	modified:   server.js 	modified:   source/Confirmed.csv 	modified:   source/Deaths.csv 	modified:   source/Recovered.csv

--- parser.js
+++ parser.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const csv = require('neat-csv');
+const isDate = require('./tools/isDate');
 
 module.exports = async function(conf,rec,ded){
     let rawData = {};
@@ -14,33 +15,72 @@ module.exports = async function(conf,rec,ded){
     rawData.recovered = await csv(recoveredCsv);
     rawData.deaths = await csv(deathsCsv);
     
-    rawData.confirmed.forEach(item => {
-        if (!countries.includes(item['Country/Region'])){
-            countries.push(item['Country/Region']);
-            finalData.push({name:item['Country/Region'],toll:0,recovered:0,deaths:0,sick:0});
+
+    let i = 1;
+    rawData.confirmed.forEach((item) => {
+        if (!countries.some(e => e.name === item['Country/Region'])){
+            countries.push({id:i,name:item['Country/Region']});
+            let newCountry = {id:i,name:item['Country/Region'],toll:0,recovered:0,deaths:0,sick:0,history:{toll:{},recovered:{},deaths:{},sick:{}}};
+            let entries = Object.entries(item);
+            // further optimisation needed...
+            // make a 'dates' array ??
+            entries.forEach(([key,val])=>{
+                if(isDate(key)) {
+                    newCountry.history.toll[key] = 0;
+                    newCountry.history.recovered[key] = 0;
+                    newCountry.history.deaths[key] = 0;
+                    newCountry.history.sick[key] = 0;
+                }
+            })
+            finalData.push(newCountry);
+            i++;
         };
     })
+
     finalData.forEach(country => {
         rawData.confirmed.forEach(item=>{
             if (item['Country/Region'] === country.name){
-                let arr = Object.values(item);
-                country.toll += Number(arr[arr.length-1]);
+                let entries = Object.entries(item);
+                entries.forEach(([key,val])=>{
+                    if(isDate(key)){
+                        country.history.toll[key] += Number(val);
+                    }
+                })
+                let values = Object.values(item);
+                country.toll += Number(values[values.length-1]);
             }
         })
         rawData.recovered.forEach(item=>{
             if (item['Country/Region'] === country.name){
-                let arr = Object.values(item);
-                country.recovered += Number(arr[arr.length-1]);
+                let entries = Object.entries(item);
+                entries.forEach(([key,val])=>{
+                    if(isDate(key)){
+                        country.history.recovered[key] += Number(val);
+                    }
+                })
+                let values = Object.values(item);
+                country.recovered += Number(values[values.length-1]);
             }
         })
         rawData.deaths.forEach(item=>{
             if (item['Country/Region'] === country.name){
-                let arr = Object.values(item);
-                country.deaths += Number(arr[arr.length-1]);
+                let entries = Object.entries(item);
+                entries.forEach(([key,val])=>{
+                    if(isDate(key)){
+                        country.history.deaths[key] += Number(val);
+                    }
+                })
+                let values = Object.values(item);
+                country.deaths += Number(values[values.length-1]);
             }
         })
-        country.sick = country.toll - country.recovered;
-    })
+        country.sick = country.toll - (country.recovered + country.deaths);
+        for (let [date,toll] of Object.entries(country.history.sick)){
+            country.history.sick[date] = country.history.toll[date] - (country.history.recovered[date]+country.history.deaths[date]);
+        }
 
+        if(country.name === "US") country.name = "Diamond Princess cruise ship";
+    })
+   
     return {data:finalData,countries};
 }
--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const parse = require('./parser');
-
-let finalData = [];
+const finalData = require('./all.json')
 
 const sourceUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-';
 let fileNames = ['Confirmed.csv','Recovered.csv','Deaths.csv'];
@@ -28,16 +27,18 @@ function getAll(){
     Promise.all(requests).then(()=>{
         parse('./source/Confirmed.csv','./source/Recovered.csv','./source/Deaths.csv').then(data=>{
             finalData = data;
+            console.log("bootstrapping finished");
         })
     })
 }
-getAll();
 
-// Updates source csv files once every 12h
-setInterval(()=>{
-    getAll();
-    console.log('data source updated.')
-},43200000);
+// let finalData = [];
+// getAll();
+// // Updates source csv files once every 12h
+// setInterval(()=>{
+//     getAll();
+//     console.log('data source updated.')
+// },43200000);
 
 let app = express();
 
@@ -46,6 +47,12 @@ app.use(cors());
 app.get("/all", (req, res) => {
     res.json(finalData.data);
 });
+
+app.get("/country/:id",(req,res)=>{
+    let id = Number(req.params.id);
+    if (id<1 || id>finalData.data.length) res.status(404);
+    res.json(finalData.data[id-1]);
+})
 
 app.get("/locations",(req,res)=>{
     res.json(finalData.countries);
